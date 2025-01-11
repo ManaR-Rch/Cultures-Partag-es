08390feb4c33218cec62d3736f07eea30dbc9928
ManaR-Rch <manarmarchou6@gmail.com>
2025-01-11 11:44:55 +0100

    ajouter file mailer

---
+++ Views/Auth/mailer.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php'; // Charger l'autoloader de Composer
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class Mailer {
+    private $mail;
+
+    public function __construct() {
+        $this->mail = new PHPMailer(true);
+    }
